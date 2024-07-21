66b9012bfc3fa61834c23db04bbd2b6f83e0af8e
Muhammad Hamza Saeed <89595607+mhsdotdev@users.noreply.github.com>
2024-07-21 16:45:01 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
     // Fetch book data from books.json
     $.ajax({
-        url: 'books.json',
+        url: 'https://localhost:7187/api/books',
         type: 'GET',
         dataType: 'json',
         success: function(data) {
@@ -13,25 +13,78 @@ $(document).ready(function() {
     });
 
     // Search functionality
+    // $("#search-button").on("click", function() {
+    //     var searchTerm = $("#search-bar").val().toLowerCase();
+    //     var filteredBooks = books.filter(function(book) {
+    //         return book.title.toLowerCase().includes(searchTerm) ||
+    //                book.author.toLowerCase().includes(searchTerm);
+    //     });
+    //     displayResults(filteredBooks);
+    // });
+
+    // View All functionality
     $("#viewAll").on("click", function() {
         displayResults(books);
     });
-
 
     function displayResults(results) {
         var resultsContainer = $(".gallery");
         resultsContainer.empty();
 
         if (results.length > 0) {
-            var resultsHtml = "<div class=book><br>";
             results.forEach(function(book) {
-                resultsHtml += "<img src=" + book.image + "><br>";
-                resultsHtml += "<p><h4>" + book.title + "</h><p><br>";
-                resultsHtml += "<p>" + book.author + "</p><br>";
+                var bookHtml = "<div class='book'>";
+                bookHtml += "<img src='" + book.image + "'>";
+                bookHtml += "<div class =description>";
+                bookHtml += "<p><h5>" + book.title + "</h5><p>";
+                bookHtml += "<p>" + book.author + "</p>";
+                bookHtml += "<p>" + book.desc + "</p>";
+                bookHtml += "</div>";
+                bookHtml += "</div>";
+                resultsContainer.append(bookHtml);
             });
-            resultsHtml+="</div><br>";
-            resultsContainer.html(resultsHtml);
-            console.log(resultsHtml);
+        } else {
+            resultsContainer.html("<p>No results found.</p>");
+        }
+    }
+});
+
+$(document).ready(function() {
+    $('#search-button').click(function() {
+        var author = $('#search-bar').val();
+        fetchBooksByAuthor(author);
+    });
+
+    function fetchBooksByAuthor(author) {
+        $.ajax({
+            url: `https://localhost:7187/api/books/author/${author}`,
+            method: 'GET',
+            success: function(data) {
+                displayData(data);
+            },
+            error: function(err) {
+                console.error('Error fetching data:', err);
+                $('#data-container').html('<p>No books found for the specified author.</p>');
+            }
+        });
+    }
+
+    function displayResults(results) {
+        var resultsContainer = $(".gallery");
+        resultsContainer.empty();
+
+        if (results.length > 0) {
+            results.forEach(function(book) {
+                var bookHtml = "<div class='book'>";
+                bookHtml += "<img src='" + book.image + "'>";
+                bookHtml += "<div class =description>";
+                bookHtml += "<p><h5>" + book.title + "</h5><p>";
+                bookHtml += "<p>" + book.author + "</p>";
+                bookHtml += "<p>" + book.desc + "</p>";
+                bookHtml += "</div>";
+                bookHtml += "</div>";
+                resultsContainer.append(bookHtml);
+            });
         } else {
             resultsContainer.html("<p>No results found.</p>");
         }
